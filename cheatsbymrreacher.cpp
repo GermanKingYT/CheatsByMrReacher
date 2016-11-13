8ae83281acac94a6215ec1423a1eadd1b7f985ce
Edy <support@mrreacher.com>
2016-11-13 17:08:25 +0200

    Create cheatsbymrreacher.cpp

---
+++ cheatsbymrreacher.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+int value;
+
+cout << "Press 1 if you want to activate CheatsByMrReacher.com!" << endl;
+cin >> value;
+
+if(value == 1) {
+    cout << "\n";
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "CheatsByMrReacher.com Activated." << endl;
+    cout << "\n";
+    system("PAUSE");
+}
+else {
+    do {
+        system ("start notepad.exe"); // Acesta este un loop pentru a deschide notepad-ul pana cand programul fa vii inchis.
+    } while(value > 1); // Se va activa daca valoarea introdusa va fii orice alt numar in afara de 1.
+
+return 0;
+}
+}
